010de89276ccbacbcbd3d00bbf97a31f3b705995
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-06 15:11:57 +0530

    [Jyoti Rao] Given three Integers find the maximum

---
+++ FindMaximumProblem/MaximumNumberCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumProblem
+{
+    public class MaximumNumberCheck
+    {
+        /// <summary>
+        /// This method is used to find interger maximum number.
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <param name="thirdValue"></param>
+        /// <returns></returns>
+        public static int MaxIntegerNumberCheck(int firstValue,int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue ,SecondValue and thirdValue are same");
+        }
+    }
+}
---
+++ FindMaximumProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FindMaximumProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Find Maximum Problems Using Generics");
+            Console.WriteLine("Please select given options");
+            Console.WriteLine("1.FindIntergerMaximumNumber\n2.Exit\n");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Please enter three values to find maximum number:");
+                    int firstValue = Convert.ToInt32(Console.ReadLine());
+                    int secondValue = Convert.ToInt32(Console.ReadLine());
+                    int thirdValue = Convert.ToInt32(Console.ReadLine());
+                    int output = MaximumNumberCheck.MaxIntegerNumberCheck(firstValue, secondValue, thirdValue);
+                    Console.WriteLine("{0} is the maximum number", output);
+                    break;
+                case 2:
+                    Console.Clear();
+                    break;
+                default :
+                    Console.Clear();
+                    Console.WriteLine("Please select only given options");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
